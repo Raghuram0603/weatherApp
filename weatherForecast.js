9a1af87a6ff67a906b07538ab98ac122395633e4
Raghuram0603 <raghuram060303@gmail.com>
2023-02-07 15:02:48 +0530

    units changed

--- weatherForecast.js
+++ weatherForecast.js
@@ -1,13 +1,13 @@
 function weather() {
     let city = document.getElementById("city")
-    let response1 = fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=930a1ba727a99be0ffe9c24abd87902d")
+    let response1 = fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=930a1ba727a99be0ffe9c24abd87902d&units=metric")
     response1.then((res) => {
         res.json().then((data) => {
             let para = document.getElementById("para");
             para.innerHTML="<br>Current Temperature : "+data.main.temp+"<br><br>"+"Maximum Temperature : "+data.main.temp_max+"<br><br>"+"Minimum Temperature : "+data.main.temp_min+"<br><br><br>"
         })
     })
-    let response2 = fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city.value + "&appid=930a1ba727a99be0ffe9c24abd87902d")
+    let response2 = fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city.value + "&appid=930a1ba727a99be0ffe9c24abd87902d&units=metric")
     response2.then((res) => {
         res.json().then((data) => {
             let j = 0;
